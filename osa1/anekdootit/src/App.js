7cd2f6be2f96188f548e420b6e7a6a33212eb17a
Anna Suuronen <annasuuronen@icloud.com>
2023-01-30 18:15:20 +0200

    tehtävät 1.13 ja 1.14 tehty

--- osa1/anekdootit/src/App.js
+++ osa1/anekdootit/src/App.js
@@ -1,5 +1,13 @@
 import { useState } from 'react'
 
+const MostVoted = ({votes, setNew, setBiggerVote, mostVoted, biggestVote, anecdotes}) => {
+  return (
+    <div>
+      <p>{mostVoted}</p>
+      <p>has {biggestVote} votes</p>
+    </div>
+  )
+}
 
 const App = () => {
   const anecdotes = [
@@ -13,13 +21,34 @@ const App = () => {
     'The only way to go fast, is to go well.'
   ]
    
+  const list = new Uint8Array(anecdotes.length)
   const [selected, setSelected] = useState(0)
+  const [votes, setVote] = useState(list)
+  const [mostVoted, setNew] = useState(anecdotes[0])
+  const [biggestVote, setBiggerVote] = useState(0)
+  const [currentVote, setCurrent] = useState(votes[0])
+
+  const vote = () => {
+    var newVotes = votes
+    votes[selected] = votes[selected] + 1
+    setVote(newVotes)
+    setCurrent(votes[selected])
+    if (votes[selected] > biggestVote) {
+      setBiggerVote(votes[selected])
+      setNew(anecdotes[selected])
+    }
+  }
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       {anecdotes[selected]}
+      <p>has {votes[selected]} votes</p>
       <br></br>
+      <button onClick={vote}>vote</button>
       <button onClick={() => setSelected(Math.floor(Math.random() * anecdotes.length))}>next anecdote</button>
+      <h1>Anecdote with most votes</h1>
+      <MostVoted votes={votes} setNew={setNew} setBiggerVote={setBiggerVote} mostVoted={mostVoted} biggestVote={biggestVote} anecdotes={anecdotes} />
     </div>
   )
 }
