223d525d6f6c0d93e5e1efe31c1dc1813b41cfb6
Anna Suuronen <annasuuronen@icloud.com>
2023-02-06 18:36:03 +0200

    tehtävä 2.6 tehty

---
+++ osa2/puhelinluettelo/src/App.js
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+
+const Numbers = (props) => {
+  return (
+    <div>
+      <h2>Numbers</h2>
+      {props.numbers.map(number => <Number key={number.name} person={number.name} /> )}
+    </div>
+
+  )
+}
+
+const Number = (props) => {
+  return (
+    <p>{props.person}</p>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const addNewNumber = (event) => {
+    event.preventDefault()
+    const numberObject = {
+      name: newName
+    }
+    setPersons(persons.concat(numberObject))
+    setNewName('')
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNewNumber}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <Numbers numbers={persons}/>
+    </div>
+  )
+
+}
+
+export default App
